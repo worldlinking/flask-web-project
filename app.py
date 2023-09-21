abb04a8ee075d4d31fcfb2c55e018b1e280f77b1
worldlinking <93902460+worldlinking@users.noreply.github.com>
2023-09-21 06:44:33 +0000

    add dynamic data and API

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
@@ -27,6 +27,11 @@ JOBS = [
 @app.route("/")
 def hello_world():
   return render_template('home.html', jobs=JOBS)
+
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 
 if __name__ == "__main__":
