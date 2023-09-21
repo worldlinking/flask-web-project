e52417347004aaaf9c6933de27b909c9be3550b0
worldlinking <93902460+worldlinking@users.noreply.github.com>
2023-09-21 06:33:33 +0000

    add html and css

--- app.py
+++ app.py
@@ -1,11 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+JOBS = [
+    {
+        'id': 1,
+        'title': 'Data Analyst',
+        'location': 'India',
+        'salary': '150,000'
+    },
+    {
+        'id': 2,
+        'title': 'Frontend Engineer',
+        'location': 'India',
+        'salary': '150,000'
+    },
+    {
+        'id': 3,
+        'title': 'Backend Engineer',
+        'location': 'India',
+        'salary': '150,000'
+    },
+]
 
 
 @app.route("/")
 def hello_world():
-  return "Hello,joy"
+  return render_template('home.html', jobs=JOBS)
 
 
 if __name__ == "__main__":
